3d4367221b24cf367747c9854dc2d1255d60c670
Bant89 <bant8009@gmail.com>
2020-06-01 10:59:18 -0400

    Fetch cities data

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, FormEvent } from 'react'
 
+import { BASE_URL, API_KEY } from './utils/index'
+import { ErrorMessage } from './utils/types'
 
 export const Main = () => {
   const [input, setInput] = useState('')
-  
+  const [responses, setResponses] = useState<Array<{}>>([])
 
-  const handleChange = (e: { target: { value: React.SetStateAction<string> } }) => {
-    setInput(e.target.value)
+  const handleChange = (e: FormEvent<HTMLInputElement>) => {
+    setInput(e.currentTarget.value)
+  }
+
+  const makeRequest = (value: string) => {
+    fetch(`${BASE_URL}${value}${API_KEY}`)
+      .then(res => res.json())
+      .then(data => setResponses(state => [...state, data]))
+      .catch(error => console.error(error))
   }
 
   const handleClick = () => {
-    console.log(input)
+    input.split(',')
+    .map(e => sanitizeInput(e))
+    .forEach(e => makeRequest(e))
   }
 
   return(
@@ -22,3 +33,23 @@ export const Main = () => {
   )
 }
 
+
+export const sanitizeInput = (value: string) => {
+  const letterRegex = /[A-Za-z]/g,
+    digitRegex = /\d/g
+
+  let returnValue = value.trim()
+  if(returnValue.length === 0){
+    returnValue = "Value can't be empty"
+  }else if(digitRegex.test(value) && letterRegex.test(value)){
+    const letterAmount = value.match(letterRegex)!.length
+    const digitAmount = value.match(digitRegex)!.length
+    
+    if(letterAmount >= digitAmount) {
+      returnValue = "City name shouldn't include numbers"
+    }else {
+      returnValue = "Postal code should include letters"
+    }
+  }
+  return returnValue
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?q='
+export const API_KEY = `&APPID=${process.env.REACT_APP_API_KEY}`
